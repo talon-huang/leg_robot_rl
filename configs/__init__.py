178fb623f700ef610aac71cd10d56eaedd786c0e
talon-huang <huang_xi_tao@outlook.com>
2025-03-22 01:01:38 +0800

    add cyberdog urdf

---
+++ configs/__init__.py
@@ -0,0 +1,3 @@
+from .base_config import *
+from .legged_robot_config import *
+from .titati_constaint_config import *
---
+++ train.py
@@ -0,0 +1,22 @@
+import numpy as np
+import os
+from datetime import datetime
+from configs.titati_constaint_config import TitatiConstraintHimRoughCfg, TitatiConstraintHimRoughCfgPPO
+
+
+import isaacgym
+from utils.helpers import get_args
+from envs import LeggedRobot
+from utils.task_registry import task_registry
+
+def train(args):
+    env, env_cfg = task_registry.make_env(name=args.task, args=args)
+    ppo_runner, train_cfg = task_registry.make_alg_runner(env=env, name=args.task, args=args)
+    ppo_runner.learn(num_learning_iterations=train_cfg.runner.max_iterations, init_at_random_ep_len=True)
+
+if __name__ == '__main__':
+
+    task_registry.register("Titatit",LeggedRobot,TitatiConstraintHimRoughCfg(),TitatiConstraintHimRoughCfgPPO())
+
+    args = get_args()
+    train(args)
